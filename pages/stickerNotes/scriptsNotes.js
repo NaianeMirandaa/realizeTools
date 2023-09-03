bd6f8955e9627f771c1e238d0fdc061912b4897e
Naiane Miranda de Jesus <naianemjesus@gmail.com>
2023-09-02 21:03:45 -0300

    mudanças no stickerNotes

---
+++ pages/stickerNotes/scriptsNotes.js
@@ -0,0 +1,6 @@
+let createBox = document.getElementsByClassName('createBox');
+let create = document.getElementById('create');
+
+create.addEventListener('click', function(){
+    createBox.style.display = 'block';
+});
